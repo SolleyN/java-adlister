13f82dd1ba2e21714deae987e984eb5bbbb1214a
Nickolas Solley <nicksolley@gmail.com>
2023-03-13 13:49:28 -0500

    guess

--- src/main/java/GuessServlet.java
+++ src/main/java/GuessServlet.java
@@ -1,21 +1,26 @@
-import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Random;
 
+@WebServlet("/guess")
 public class GuessServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/guess.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        int guess = Integer.parseInt(request.getParameter("guess"));
-        if (guess == 2) {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Random random = new Random();
+        int rand =  random.nextInt(3 - 1) + 1;
+        System.out.println("random is " + rand);
+        String guess = request.getParameter("guess");
+        System.out.println(guess);
+        if (Integer.parseInt(guess) == rand) {
             response.sendRedirect("/correct");
         } else {
             response.sendRedirect("/incorrect");
---
+++ src/main/webapp/result.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: nickolassolley
+  Date: 3/13/23
+  Time: 11:12
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>Result Page</title>
+</head>
+<body>
+
+<h1>Result: ${result}</h1>
+
+</body>
+</html>
